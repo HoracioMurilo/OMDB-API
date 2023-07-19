c070f383a9c1363d7c193153a29d616f5a75fc73
Murilo <101604089+HoracioMurilo@users.noreply.github.com>
2023-07-18 23:06:44 -0300

    Aula 14 de 25: inteligência da Página Principal

---
+++ quero_assistir/script.js
@@ -0,0 +1,10 @@
+const searchButton = document.getElementById("search-button");
+const overlay = document.getElementById("modal-overlay");
+
+function searchButtonClickHandler() {
+    overlay.classList.add('open');
+}
+
+searchButton.addEventListener('click', searchButtonClickHandler); 
+
+
