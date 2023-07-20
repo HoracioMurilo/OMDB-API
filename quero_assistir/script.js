f7e8100fbe6d68224670f0ddcf8df38617004007
Murilo <101604089+HoracioMurilo@users.noreply.github.com>
2023-07-19 22:31:10 -0300

    Aula 15 de 25: Capturando as entradas do usuário
    const movieNameValue = movieName.value.split(' ').join('+');

--- quero_assistir/script.js
+++ quero_assistir/script.js
@@ -1,8 +1,14 @@
 const searchButton = document.getElementById("search-button");
 const overlay = document.getElementById("modal-overlay");
+const movieName = document.getElementById("movie-name");
+const movieYear = document.getElementById("movie-year");
 
 function searchButtonClickHandler() {
     overlay.classList.add('open');
+    const movieNameValue = movieName.value.split(' ').join('+');
+    const movieYearValue = movieYear.value;
+    console.log(movieNameValue);
+    console.log(movieYearValue);
 }
 
 searchButton.addEventListener('click', searchButtonClickHandler); 
