07a721656484ad1b9fb921cba5ac214313997d99
Murilo <101604089+HoracioMurilo@users.noreply.github.com>
2023-07-19 23:49:48 -0300

    Aula 17 de 25: Lidando com erros
    Blocos Try Catch para tratar chamadas erradas da API

--- quero_assistir/script.js
+++ quero_assistir/script.js
@@ -4,16 +4,34 @@ const movieName = document.getElementById("movie-name");
 const movieYear = document.getElementById("movie-year");
 
 async function searchButtonClickHandler() {
-  overlay.classList.add("open");
-  let url = `http://omdbapi.com/?apikey=${key}&t=${movieName.value
-    .split(" ")
-    .join("+")}&${movieYear.value}`;
-
+  try {
+    let url = `http://omdbapi.com/?apikey=${key}&t=${movieNameParameterGenerator()}&y=${movieYearParameterGenerator()}`;
     const response = await fetch(url);
     const data = await response.json();
     console.log(data);
+    overlay.classList.add("open");
+  } catch (error) {
+    console.error(error.message);
+  }
+}
 
-    
+function movieNameParameterGenerator() {
+  if (movieName.value === "") {
+    throw new Error("Movie name is empty");
+  }
+  return movieName.value.split(" ").join("+");
+}
+
+function movieYearParameterGenerator() {
+  if (movieYear.value === "") {
+    throw new Error("Movie year is empty");
+  }
+
+  if (movieYear.value.length !== 4 || isNaN(movieYear.value)) {
+    throw new Error("Movie year is invalid");
+  }
+
+  return movieYear.value;
 }
 
 searchButton.addEventListener("click", searchButtonClickHandler);
