c40d1defa50df900dae7cd120997218b341a932e
Murilo <101604089+HoracioMurilo@users.noreply.github.com>
2023-07-19 22:52:24 -0300

    Aula 16 de 25: Consumindo a API do OMDB
    Finalmente Consumindo a API e recebendo resposta:
    
      let url = `http://omdbapi.com/?apikey=${key}&t=${movieName.value
        .split(" ")
        .join("+")}&${movieYear.value}`;
    
        const response = await fetch(url);
        const data = await response.json();
        console.log(data);

--- quero_assistir/script.js
+++ quero_assistir/script.js
@@ -3,14 +3,17 @@ const overlay = document.getElementById("modal-overlay");
 const movieName = document.getElementById("movie-name");
 const movieYear = document.getElementById("movie-year");
 
-function searchButtonClickHandler() {
-    overlay.classList.add('open');
-    const movieNameValue = movieName.value.split(' ').join('+');
-    const movieYearValue = movieYear.value;
-    console.log(movieNameValue);
-    console.log(movieYearValue);
+async function searchButtonClickHandler() {
+  overlay.classList.add("open");
+  let url = `http://omdbapi.com/?apikey=${key}&t=${movieName.value
+    .split(" ")
+    .join("+")}&${movieYear.value}`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+
+    
 }
 
-searchButton.addEventListener('click', searchButtonClickHandler); 
-
-
+searchButton.addEventListener("click", searchButtonClickHandler);
